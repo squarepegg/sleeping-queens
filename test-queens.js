b3be72605a630cdf816024fc6f09f1d705cc9e4c
Nic St. Pegg <nic.stpegg@fortisgames.com>
2025-09-15 11:07:09 -0700

    WIP

--- test-queens.js
+++
@@ -1,7 +0,0 @@
-// Test queens initialization
-const { GameEngine } = require('./dist/game/engine/GameEngine');
-const game = new GameEngine();
-const state = game.getState();
-
-console.log('Initial sleeping queens count:', state.sleepingQueens.length);
-console.log('Queens:', state.sleepingQueens.map(q => q.name));
